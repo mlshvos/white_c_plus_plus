38c10aec968af8463d6f8681065f88f9b8c1d5e4
ilyalyov <ilyalyov@yandex-team.ru>
2022-03-10 22:56:45 +0300

    task_8

---
+++ week_1/task_8.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int x, y;
+
+    cin >> x >> y;
+
+    if (x % y == 0) {
+        cout << x;
+    } else if (y % x == 0) {
+        cout << y;
+    } else {
+        int end = 2;
+        int start = (int) sqrt(x);
+        for (int i = end; i >= start; i -= 1) {
+            if (x % i == 0 && y % i == 0) {
+                cout << i;
+            }
+        }
+    }
+
+    return 0;
+}
