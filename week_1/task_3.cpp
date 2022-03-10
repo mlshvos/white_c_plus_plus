2e3f405016e15427fffc00678cddf2533f3b36e9
ilyalyov <ilyalyov@yandex-team.ru>
2022-03-10 21:43:31 +0300

    fix linear key

--- week_1/task_3.cpp
+++ week_1/task_3.cpp
@@ -7,11 +7,17 @@ int main() {
     double a, b, c;
     cin >> a >> b >> c;
     
-    double D = b * b - 4 * a * c;
-    if (D == 0) {
-        cout << -b / 2 / a;
-    } else if (D > 0) {
-        cout << (-b + sqrt(D)) / 2 / a << " " << (-b - sqrt(D)) / 2 / a;
+    if (a == 0) {
+        if (b != 0) {
+            cout << -c / b;
+        }
+    } else {
+        double D = b * b - 4 * a * c;
+        if (D == 0) {
+            cout << -b / 2 / a;
+        } else if (D > 0) {
+            cout << (-b + sqrt(D)) / 2 / a << " " << (-b - sqrt(D)) / 2 / a;
+        }
     }
 
     return 0;
