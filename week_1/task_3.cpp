55fd289bd35b2b3024ce61779cca4292958f3d21
ilyalyov <ilyalyov@yandex-team.ru>
2022-03-10 21:30:57 +0300

    task_3

---
+++ week_1/task_3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double a, b, c;
+    cin >> a >> b >> c;
+    
+    int D = b * b - 4 * a * c;
+    if (D == 0) {
+        cout << -b / 2 / a;
+    } else if (D > 0) {
+        cout << (-b + sqrt(D)) / 2 / a << (-b + sqrt(D)) / 2 / a;
+    }
+
+    return 0;
+}
