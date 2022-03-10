0cc3c0e841bc029ca3113d15a2f52c2441562146
ilyalyov <ilyalyov@yandex-team.ru>
2022-03-10 22:25:04 +0300

    task_4

--- examples/operations_with_containers.cpp
+++ examples/operations_with_containers.cpp
@@ -16,6 +16,21 @@ int main() {
         cout << c << ";";
     }
 
+    map<string, int> b = {{"a", 1}, {"b", 2}, {"c", 3}};
+
+    int sum = 0;
+    string concat;
+    for (auto i : b) {
+        concat += i.first;
+        sum += i.second;
+    }
+    
+    int i = 10;
+    do {
+        cout << i << endl;
+        --i;
+    } while (i != 0);
+
     vector<int> nums = {1, 5, 2, 3, 5};
     int quantity = count(begin(nums), end(nums), 5); //count, sort from algorithm
     sort(begin(nums), end(nums));
