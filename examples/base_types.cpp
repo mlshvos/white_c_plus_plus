b55521b2c0be185de1cb31f90408ff18676731ad
ilyalyov <ilyalyov@yandex-team.ru>
2022-03-10 12:03:20 +0300

    set settings

--- examples/base_types.cpp
+++ examples/base_types.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 int main() {
+    int x = -5;
+    double pi = 3.14;
+    bool a = true;
+    char symb = 'a';
+
+    string hw = "Hello, world!";
+
+    vector<int> nums = {1, 3, 5, 7};
+    cout << nums.size();
     return 0;
 }
