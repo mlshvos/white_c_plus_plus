92dd94685d9947334e4b0d63628d0014a096dcb0
ilyalyov <ilyalyov@yandex-team.ru>
2022-03-10 23:59:05 +0300

    task_9

---
+++ week_1/task_9.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int N;
+
+    cin >> N;
+
+    vector<int> res;
+
+    while (N != 0) {
+        res.push_back(N % 2);
+        N /= 2;
+    }
+
+    for (int i = res.size() - 1; i >= 0; --i) {
+        cout << i;
+    }
+
+    return 0;
+}
